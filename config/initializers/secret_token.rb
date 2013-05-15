1d5b4dbb1df3aa64ab8ad0ff8cdc108322a1ea76
Carl Robinson <charlyblack75@gmail.com>
2013-05-16 00:04:41 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comfy4::Application.config.secret_token = '2849d2220fe04d94cd59e4ffccf0eb6c9edef33a51b15d27ee9b6f6d6d8bd2314e152f424f41fda9b9cd4779d5dfb10911cbfe87de1c5a1d4a60f3476acb77a3'
